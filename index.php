5be75cfeb5461158d84ed90a1caf3ecec31f3214
Mark Goldberg <mgoldberg@launchdm.com>
2016-05-24 13:31:37 -0400

    cleaned up code a bit

--- index.php
+++ index.php
@@ -18,10 +18,6 @@
 			<p>
 				There is an extrodinarily high chance that you have seen an animal under the category of bird before.  I just saw one fly right past my window.  That's right I said <text id = "stressed"> fly</text>!  One of the coolest things about birds is that most of them can sustain self powered flight.  Another destinctive trait that all birds have are feathers.  We'll go into more detail later, but for now just know that feathers are similar to long hairs that branch off into other hairs.  All birds also have beaks.  They com in many shapes and sizes, but they are generally pointy yellow triangles at the front of a birds head.  This is the location of the bird's mouth and nose.  Do you remeber seeing a bird yet?  Well hold onto your ostreches because there is a lot more to our feathery friends that meet the eye.
 			</p>
-		<!--</div>-->
-		
-
-		<!--<div id = "left 1">-->
 			<p>
 				If you are still confused on the concept of a bird, fret not!  Here is a list of birds.  You can search for information about any of them if you like.  As you can see, while birds come in many shapes ans sizes, there are many similarities.
 			</p>
@@ -59,9 +55,6 @@
 			<p>
 				Birds can be found on any continent on Earth.  This may be because birds can travel over vast distances very quickly through the power of flight.  No bird shows this better than the arctic tern.  Its migration brings it from the arctic circle all the way to Antactica (see picture to the right).  Another reason birds can be found in diverse locations is because they can take a variety of niches.  Birds can be everything from hunters to berry eaters.  The frigate bird gets much of its food from bothering other animals!  Many birds can even survive in the desert!  Their digestive syustem allows them to get the water they need from the food that they eat.  Many bird species will form flocks.  Different bird species will do this for different reasons.  For instance, the canadian goose stays in a flock for aerodynamics.  They will fly in a v shape, and the geese flying in the back will have less air resistance.  The geese will then, after a certain period of tiime, will take turns.  However, not all birds like flocks.  In fact, many birds are very territorial.  For instance, the cardinal will often be see in a high open place singing its song.  He isn't doing this because he is bored: he's letting every other male cardinal in the area that the area he is in is his and no one else's.
 			</p>
-			<p>
-				<!--<pre>	Birds</pre>-->
-			</p>
 		</div>
 		<img id = "right1" src="http://localhost/StaticHTML2/images/Picture4.png" height = "200px" width = "200px"/>
 
@@ -79,7 +72,7 @@
 		<div id = right2>
 			<a id = "title1">Birds in Human Culture</a>
 			<p>
-				One of the largest impact birds have had on humans is as a food sourse.  Humans really like chicken. I mean <text id = "stressed">really</text> lke chicken.  There are currently <text id = "stressed">50 billion</text> chickens in the world today.  That's seven times the number of humans in the world.  However, live birds have made there way into human culture.  For example, in the middle ages, being able to go hunting with their falcon was a large status symbol.  A famous mythical bird is the pheonix.  It is a fire bird that, when it dies, it burns up and is born from the ashes.  A modern bird symbol is the bald eagle.  It is America's national bird.  It's image even shows up in the oval office floor.  Birds have also been a symbol of freedom, as the ability to fly seems like it would allow for us to be carefree.  For some reason, birds also show up in a number of cartoons.  There is daffy duck, roadrunner, donald duck, woody woodepecker, foghorn leghorn, tweetybird, and more.
+				One of the largest impact birds have had on humans is as a food sourse.  Humans really like chicken. I mean <text id = "stressed">really</text> lke chicken.  There are currently <text id = "stressed">50 billion</text> chickens in the world today.  That's seven times the number of humans in the world.  However, live birds have also made there way into human culture.  For example, in the middle ages, being able to go hunting with their falcon was a large status symbol.  A famous mythical bird is the pheonix.  It is a fire bird that, when it dies, it burns up and is born from the ashes.  A modern bird symbol is the bald eagle.  It is America's national bird.  It's image even shows up in the oval office floor.  Birds have also been a symbol of freedom, as the ability to fly seems like it would allow for us to be carefree.  For some reason, birds also show up in a number of cartoons.  There is daffy duck, roadrunner, donald duck, woody woodepecker, foghorn leghorn, tweetybird, and more.
 			</p>
 		</div>
 
